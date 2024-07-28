1c192425ca0177f2479852ebd8e9a2386b8d944e
Debashish Shandilya <deba12011989@gmail.com>
2024-07-28 17:17:21 +0530

    jenkins eks files

---
+++ Jenkins-Server/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+
+  description = "VPC CIDR"
+  type        = string
+
+}
+variable "public_subnets" {
+  description = "Subnet CIDR"
+  type        = list(string)
+}
+
+variable "instance_type" {
+
+  description = "EC2 instance type"
+  type        = string
+
+}
